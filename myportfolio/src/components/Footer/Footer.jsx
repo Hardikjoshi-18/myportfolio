3a025802a210d19ef080d9302b2b241c71c204d3
Hardikjoshi-18 <hardikaimnda@gmail.com>
2025-04-30 16:09:41 +0530

    fifth commit

--- myportfolio/src/components/Footer/Footer.jsx
+++ myportfolio/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaYoutube} from 'react-icons/fa'
 
 function Footer() {
   const handleScroll = (sectionId) => {
@@ -32,6 +33,28 @@ function Footer() {
           )))}
 
         </nav>
+        <div className='flex flew-wrap justify-center space-x-4 mt-6'>
+          {[
+            {icon: <FaFacebook/>, link: ""},
+            {icon: <FaTwitter/>, link: ""},
+            {icon: <FaLinkedin/>, link: ""},
+            {icon: <FaInstagram/>, link: ""},
+            {icon: <FaYoutube/>, link: ""},
+          ].map((item,index)=>{
+            return<a
+            key={index}
+            href={item.link}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-xl hover:text-purple-500 transition-transform transform hover:scale-110' 
+            >
+            {item.icon}
+            </a>
+          })}
+        </div>
+        <p className='text-sm text-gray-400 mt-6'>
+          @2025 Hardik Joshi. All rights reserved.
+        </p>
       </div>
 
     </footer>
